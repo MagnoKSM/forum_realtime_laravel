92e271370946bcffc12372d4a2373ac3627138bc
Magno Sousa <magno.bds@gmail.com>
2019-03-20 17:16:17 -0300

    Threads and replies: migrations, seeds, model e controllers add

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,8 @@
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\Reply;
+use App\Thread;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +24,28 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => implode('', $faker->paragraphs),
+        'user_id' => function(){
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'body' => $faker->paragraph,
+        'user_id' => function(){
+            return factory(App\User::class)->create()->id;
+        },
+        'thread_id' => function(){
+            return factory(App\Thread::class)->create()->id;
+        }
+
     ];
 });
